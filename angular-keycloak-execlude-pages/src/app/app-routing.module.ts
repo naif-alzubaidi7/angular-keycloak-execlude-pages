6015fbad4bfcfee60b9de8479414a39e268c4951
nazubaidi <nazubaidi@tahakom.com>
2020-03-20 16:20:12 +0300

    add keycloak

--- angular-keycloak-execlude-pages/src/app/app-routing.module.ts
+++ angular-keycloak-execlude-pages/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeneralComponent } from './components/general/general.component';
+import { AppAuthGuard } from './app.authguard';
+import { SecureComponent } from './components/secure/secure.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/general', pathMatch: 'full' },
+  { path: 'general', component: GeneralComponent  },
+  { path: 'general', component: SecureComponent, canActivate: [AppAuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[AppAuthGuard]
 })
 export class AppRoutingModule { }
--- angular-keycloak-execlude-pages/src/app/app.module.ts
+++ angular-keycloak-execlude-pages/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, DoBootstrap } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './components/general/general.component';
+import { SecureComponent } from './components/secure/secure.component';
+import { environment } from 'src/environments/environment';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
+const keycloakService = new KeycloakService();
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent,
+    SecureComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: KeycloakService,
+      useValue: keycloakService
+    }
+  ],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule  implements DoBootstrap {
+  async ngDoBootstrap(app: any) {
+    const { keycloakConfig } = environment;
+
+    try {
+      await keycloakService.init({ config: keycloakConfig });
+
+      app.bootstrap(AppComponent);
+    } catch (error) {
+      console.error('Keycloak init failed', error);
+    }
+  }
+}
